50e7114f66d4bb93f9f9b4f32f2a2512889ee28d
EsraaMamoun <esraamamoun5@gmail.com>
2022-06-13 11:09:47 +0300

    done signup/signin

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,24 @@
 const router = require('express').Router();
-
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const validation = require('../validation');
 
 router.post('/signup', async (req, res) => {
+    const { error } = validation.signupValidation(req.body);
+
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const emailExist = await User.findOne({email: req.body.email});
+    if(emailExist) return res.status(400).send('Email already exists!');
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     const user = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
     try {
         const savedUser = await user.save();
@@ -14,6 +26,20 @@ router.post('/signup', async (req, res) => {
     } catch (error) {
         res.status(400).send(error);
     }
+});
+
+router.post('/signin', async (req, res) => {
+    const { error } = validation.signinValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    
+    const user = await User.findOne({email: req.body.email});
+    if(!user) return res.status(400).send('Email or password is wrong!');
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validPassword) return res.status(400).send('Email or password is wrong!');
+
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
 });
 
 module.exports = router;
---
+++ validation.js
@@ -0,0 +1,36 @@
+// Validation
+const Joi = require('@hapi/joi');
+
+const signupValidation = data => {
+    const schema = Joi.object({
+        username: Joi.string()
+            .min(5)
+            .required(),
+        email: Joi.string()
+            .min(10)
+            .required()
+            .email(),
+        password: Joi.string()
+            .min(7)
+            .required()
+    });
+    return schema.validate(data);
+};
+
+const signinValidation = data => {
+    const schema = Joi.object({
+        email: Joi.string()
+            .min(10)
+            .required()
+            .email(),
+        password: Joi.string()
+            .min(7)
+            .required()
+    });
+    return schema.validate(data);
+};
+
+module.exports = {
+    signupValidation,
+    signinValidation
+}
